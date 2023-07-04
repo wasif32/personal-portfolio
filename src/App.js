9de9d8bd6ef95e5ea5e7118d56b3567022eb310a
wasif32 <101009838+wasif32@users.noreply.github.com>
2023-07-05 00:32:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} exact></Route>
           <Route path="/projects" element={<Projects />}></Route>
           <Route path="/project/:id" element={<ProjectDisplay />}></Route>
           <Route path="/experience" element={<Experience />}></Route>
